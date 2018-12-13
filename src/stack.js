7e76d8ddc6d3c587d6bffc67ab03fe94fc4c958f
Takuma Uda <hennin.ltn@gmail.com>
2018-12-13 14:25:25 +0900

    fix the order of arguments

--- src/stack.js
+++ src/stack.js
@@ -5,7 +5,7 @@ const drop = ([_, ...xs]) => xs
 const swap = ([x1, x2, ...xs]) =>
   x1 === void 0 ? [] : x2 === void 0 ? [x1] : [x2, x1, ...xs]
 
-const applyN = (n, f) => (stack) => [f(...stack.slice(0, n)), ...stack.slice(n)]
+const applyN = (n, f) => (stack) => [f(...stack.slice(0, n).reverse()), ...stack.slice(n)]
 
 const apply = f => applyN(1, f)
 
